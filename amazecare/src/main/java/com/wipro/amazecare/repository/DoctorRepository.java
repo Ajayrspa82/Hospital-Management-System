0124e4249d11cd2d167c8419d3d29c2515927f18
Mega <smega0903@gmail.com>
2026-02-27 14:29:48 +0530

    Patient and medical record modules....

--- amazecare/src/main/java/com/wipro/amazecare/repository/DoctorRepository.java
+++ amazecare/src/main/java/com/wipro/amazecare/repository/DoctorRepository.java
@@ -1,16 +1,16 @@
-package com.wipro.amazecare.repository;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wipro.amazecare.entity.Doctor;
-
-public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-
-    List<Doctor> findBySpecialization(String specialization);
-
-    Doctor findByEmail(String email);
-
-    boolean existsByEmail(String email);
-}
+//package com.wipro.amazecare.repository;
+//
+//import java.util.List;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import com.wipro.amazecare.entity.Doctor;
+//
+//public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+//
+//    List<Doctor> findBySpecialization(String specialization);
+//
+//    Doctor findByEmail(String email);
+//
+//    boolean existsByEmail(String email);
+//}
